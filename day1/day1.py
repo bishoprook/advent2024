32c9fbf1e8b9cf88b91ae8a7638cca797a7d295f
Kyle Woodlock <kyle.woodlock@gmail.com>
2024-12-03 14:41:07 -0500

    cleanup

--- day1/day1.py
+++ day1/day1.py
@@ -1,32 +1,59 @@
-import argparse
-import re
+def load_lists(filename: str) -> tuple[list[int], list[int]]:
+    """Load two lists from an input file and sort them. The input data is rows
+    of two columns of numbers which are not sorted.
+    
+    :param str filename: the file to open
+    :returns tuple[list[int], list[int]]: the two sorted lists"""
+    import re
+    pattern = r"(\d+)\s+(\d+)"
+    with open(filename, 'r') as file:
+        matches = [re.search(pattern, line).groups() for line in file]
+        list_a = [int(a) for a, _ in matches]
+        list_a.sort()
+        list_b = [int(b) for _, b in matches]
+        list_b.sort()
+        return (list_a, list_b)
 
-parser = argparse.ArgumentParser(prog='AOC2024-d01')
-parser.add_argument('filename')
-args = parser.parse_args()
+def total_distance(list_a: list[int], list_b: list[int]) -> int:
+    """Find the total distance between two lists. For each index, the distance
+    is defined as the difference between the values at that index. The total
+    distance is the sum of distances at each index.
 
-# Reading the input data, which is in lines of number pairs.
-pattern = r"(\d+)\s+(\d+)"
-with open(args.filename, 'r') as file:
-    matches = [re.search(pattern, line).groups() for line in file]
+    :param list[int] list_a: the first input list
+    :param list[int] list_b: the second input list
+    :returns int: the total distance between the lists
+    """
+    distances = [abs(a - b) for a, b in zip(list_a, list_b)]
+    return sum(distances)
 
-list_a = [int(a) for a, _ in matches]
-list_a.sort()
-list_b = [int(b) for _, b in matches]
-list_b.sort()
+def similarity(list_a: list[int], list_b: list[int]) -> int:
+    """Find the similarity between two lists. For each value in list_a, the
+    distance is defined as the product of the value and the number of times the
+    same value appears in list_b. The similarity is the sum of the distances
+    for each value in list_a. (Duplicates are allowed and will add to the
+    similarity.)
+    
+    :param list[int] list_a: the first input list
+    :param list[int] list_b: the second input list
+    :returns int: the similarity between the two lists"""
+    from itertools import groupby
+    counts_b = { k: len(list(v)) for k, v in groupby(list_b) }
+    products = [value * counts_b.get(value, 0) for value in list_a]
+    return sum(products)
 
-distances = [abs(a - b) for a, b in zip(list_a, list_b)]
-print("Total distance: ", sum(distances))
+# Only try to load command-line arguments if we're in non-interactive mode. If
+# imported into a REPL, we just want to test out the functions above.
+import sys
+interactive = hasattr(sys, 'ps1')
+if not interactive:
+    import argparse
 
-count_size = max(max(list_a), max(list_b)) + 1
+    # Parse the command line arguments.
+    parser = argparse.ArgumentParser(prog='AOC2024-d01')
+    parser.add_argument('filename')
+    args = parser.parse_args()
 
-counts_a = [0] * count_size
-for value in list_a:
-    counts_a[value] += 1
+    list_a, list_b = load_lists(args.filename)
 
-counts_b = [0] * count_size
-for value in list_b:
-    counts_b[value] += 1
-
-products = [idx * counts_a[idx] * counts_b[idx] for idx in range(count_size)]
-print("Result: ", sum(products))
+    print("Total distance: ", total_distance(list_a, list_b))
+    print("Similarity: ", similarity(list_a, list_b))
--- day2/day2.py
+++ day2/day2.py
@@ -1,27 +1,41 @@
-# Open the file and read line by line. Each report is a space-separated list of
-# levels.
-def load_reports(filename):
+from typing import Generator, Literal, NewType
+
+Level = NewType('Level', int)
+Report = list[Level]
+
+def load_reports(filename: str) -> Generator[Report, None, None]:
+    """Opens the file and reads reports line by line.
+    
+    :param str filename: the input file to open
+    :returns Generator[Report, None, None]: all the reports in sequence"""
     with open(filename, 'r') as file:
         for report_line in file:
             yield [int(level) for level in report_line.split()]
 
-# Return -1 for negative numbers, 1 for positive numbers, 0 for zero.
-def valence(num):
+def valence(num: int) -> Literal[-1, 0, 1]:
+    """Returns the valence of the number: -1 for negative numbers,
+    1 for positive numbers, 0 for zero.
+    
+    :param int num: the input
+    :returns int: -1 for negative, 1 for positive, 0 for zero"""
     return -1 if num < 0 else 1 if num > 0 else 0
 
-# Check if a report is safe without attempting to repair. As defined in the
-# problem statement...
-#   A report only counts as safe if both of the following are true:
-#    * The levels are either all increasing or all decreasing.
-#    * Any two adjacent levels differ by at least one and at most three.
-def is_safe(report):
+def is_safe(report: Report) -> bool:
+    """Check if a report is safe without attempting to repair. As defined in
+    the problem statement, a report is safe if both are true:
+    * The levels are either all increasing or all decreasing.
+    * Any two adjacent levels differ by at least one and at most three.
+    
+    :param Report report: the report to check
+    :returns bool: whether the report is safe"""
+
     # Find the difference between each element and the next. So for example,
     # [1, 3, 7, 6, 10] would become [2, 4, -1, 4]. This has a size 1 less than
     # the report.
     diffs = [b - a for a, b in zip(report, report[1:])]
 
-    # Can immediately declare unsafe if any diff is not within range of either
-    # [-3,-1] or [1,3].
+    # We can immediately declare unsafe if any diff is not within range of
+    # either [-3,-1] or [1,3].
     if any(diff < -3 or diff == 0 or diff > 3 for diff in diffs):
         return False
 
@@ -36,17 +50,25 @@ def is_safe(report):
     # changes between index 1-2 and 2-3.
     return all(a == b for a, b in zip(diff_valences, diff_valences[1:]))
 
-# Return all candidate versions of this report after undergoing at most one
-# removal of a single level. For example, [1, 3, 5, 7] would yield the sequence
-# [1, 3, 5, 7], [3, 5, 7], [1, 5, 7], [1, 3, 7], [1, 3, 5].
-def candidate_repairs(report):
+def candidate_repairs(report: Report) -> Generator[Report, None, None]:
+    """Gets all candidate versions of this report after undergoing at most one
+    removal of a single level. For example, `[1, 3, 5, 7]` would yield the
+    sequence `[1, 3, 5, 7]`, `[3, 5, 7]`, `[1, 5, 7]`, `[1, 3, 7]`,
+    `[1, 3, 5]`.
+    
+    :param Report report: the report to evaluate
+    :returns Generator[Report, None, None]: all valid repairs applied to
+    the report"""
     yield report
     for drop_idx in range(len(report)):
         yield [level for i, level in enumerate(report) if i != drop_idx]
 
-# Returns whether a given report is safe using the safety rules described above
-# and allowing up to one deletion of a level from the report.
-def is_safe_with_repair(report):
+def is_safe_with_repair(report: Report) -> bool:
+    """Checks whether this report is safe or it can be made safe with at most
+    one repair.
+    
+    :param Report report: the report to evaluate
+    :returns bool: whether the report is safe"""
     return any(is_safe(candidate) for candidate in candidate_repairs(report))
 
 # Only try to load command-line arguments if we're in non-interactive mode. If
@@ -62,6 +84,5 @@ if not interactive:
     parser.add_argument('filename')
     args = parser.parse_args()
 
-    reports = load_reports(args.filename)
-    safe_reports = quantify(reports, is_safe_with_repair)
+    safe_reports = quantify(load_reports(args.filename), is_safe_with_repair)
     print("Safe reports: ", safe_reports)
